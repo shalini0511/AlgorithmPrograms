5410f53f95bf96c7e5647b186765fd4f59825c5b
shalini <shalini.v@tvsnext.io>
2021-07-13 20:37:18 +0530

    [shalini]Binary search

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -3,32 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AlgorithmPrograms
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
-            Console.WriteLine("1.Permutation Using Recursion");
-            Console.WriteLine("Enter the option");
-            int num = Convert.ToInt32(Console.ReadLine());
-            switch (num)
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                case 1:
-                    Console.WriteLine("Enter String to find Permutation ");
-                    string str = Console.ReadLine();
-                    int n = str.Length;
-                    Permutations.Permutate(str, 0, n - 1);
-                    break;
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("3.Binary Search the word from wordlist");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permutate(str, 0, n - 1);
+                        break;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"C:\Users\HP\source\repos\AlgorithmPrograms\AlgorithmPrograms\algo.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
 
-                default:
-                    Console.WriteLine("Enter the valid option!");
-                    break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
             }
-            Console.WriteLine("\nDo you want to continue");
-            Console.ReadLine();
         }
     }
 }
