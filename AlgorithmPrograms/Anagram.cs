e424e26f804c064684e22c07b4ce6742f72a2dc7
shalini <shalini.v@tvsnext.io>
2021-07-15 15:41:01 +0530

    [shalini]Finding Anagram

---
+++ AlgorithmPrograms/Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class Anagram
+    {
+        public static void FindAnagram()
+        {
+            Console.WriteLine("Enter the String 1 :");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter the String 2 :");
+            string str2 = Console.ReadLine();
+            char[] ch1 = str1.ToUpper().ToCharArray();
+            char[] ch2 = str2.ToUpper().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string newString1 = new string(ch1);
+            string newString2 = new string(ch2);
+            if (newString1 == newString2)
+            {
+                Console.WriteLine("Both the strings are anagrams!");
+
+            }
+            else
+            {
+                Console.WriteLine("Both the strings are not anagrams!");
+            }
+        }
+    }
+}
